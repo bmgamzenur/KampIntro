6e92408d610e9d307d5315746d81863a8ba1cd49
gamze.akkas <gamze.akkas@AC0000899-0072>
2021-08-18 17:29:55 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Array - Dizi
+            string[] kurslar = new string[] {"Java","c#","pyton","android"};
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+
+            }
+            Console.WriteLine("For Ending..");
+
+            foreach (string kurs in kurslar)
+            {   
+                //foreach ile dizinin elemanlarını tek tek dolaşıp işlem yapıyor. 
+                Console.WriteLine(kurs);
+            }
+            //for daha genel amaçlı kullanılırken foreach dizileri dolaşmak için kullanılır.
+
+            Console.WriteLine("Sayfa sonu- footer");
+        }
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dört İşlem Yapma");
+
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5, 6);
+        }
+    }
+}
