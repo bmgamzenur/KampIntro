2c088a8c1e7e8b0641dd38777ffa3051c1bf6459
bmgamzenur <bmgamzenur@gmail.com>
2021-10-13 16:39:14 +0300

    ClassMetodDemo 3.Gün Ödev3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+
+        }
+        public void Delete(Musteri musteri)
+        {
+
+        }
+
+        public  void GetAllMusteri()
+        {
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri()
+            {
+                Id = 1,
+                Adi = "Gamze",
+                Soyadi = "Nur",
+                Adresi = "Ankara",
+            };
+            Musteri musteri2 = new Musteri()
+            {
+                Id = 2,
+                Adi = "Ayşe",
+                Soyadi = "Can",
+                Adresi = "İstanbul",
+            };
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Delete(musteri2);
+            musteriManager.GetAllMusteri();
+        }
+    }
+}
