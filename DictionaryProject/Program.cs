7b329ed9d732c99e2ec6f3c0b53681491211c2f1
bmgamzenur <bmgamzenur@gmail.com>
2021-10-14 16:33:52 +0300

    Dictionary Example

---
+++ DictionaryProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<String, int> AdYas = new Dictionary<string,int>();
+
+            SortedDictionary<String, int> AdYas = new SortedDictionary<string, int>();
+            AdYas.Add("Gamze", 30);
+            AdYas.Add("Ayşe", 29);
+            AdYas.Add("Nur", 33);
+            AdYas.Add("Can", 38);
+            Console.WriteLine("------------------Eski Liste");
+
+            foreach (var kisi in AdYas)
+            {
+                Console.WriteLine(kisi);
+
+            }
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Toplam Kişi sayısı: " + AdYas.Count);
+
+            AdYas.Remove("Can");
+            Console.WriteLine("------------------Yeni Liste");
+            foreach (var kisi in AdYas)
+            {
+                
+                Console.WriteLine(kisi);
+
+            }
+
+        }
+    }
+}
