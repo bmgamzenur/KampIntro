a4ea687c05250d98bfefe0f4b2258f1f00cc9cf8
bmgamzenur <bmgamzenur@gmail.com>
2021-10-15 17:18:24 +0300

    15.10.2021 haftasonu

---
+++ TestProjects/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TestProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonManager personManager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Gamze",
+                LastName = "Nur",
+                Address = "Ankara"
+            };
+            personManager.Add(customer);
+
+            personManager.AddPerson()
+        }
+        interface IPerson
+        {
+            int Id { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
+        }
+        class Customer : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+
+        }
+        class Student : IPerson
+        {
+            public int Id { get; set ; }
+            public string FirstName { get; set ; }
+            public string LastName { get ; set ; }
+            public string Department { get; set; }
+
+        }
+
+        class PersonManager
+        {
+            public void Add(Customer customer)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+            public void AddPerson(IPerson person)
+            {
+                Console.WriteLine(per.FirstName);
+            }
+
+
+        }
+    }
+}
